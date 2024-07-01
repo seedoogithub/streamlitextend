cb50be7ebc37351ad6cbadc052631678fed25cf7
AlexanderSeeDoo <alex@seedoo.co>
2024-07-01 11:51:27 +0300

    modify websocket wrapper

--- seedoo/streamlit/frontend/src/WebSocketWrapper.js
+++ seedoo/streamlit/frontend/src/WebSocketWrapper.js
@@ -1,122 +1,131 @@
 import * as msgpack from '@msgpack/msgpack';
-let start
+
 class WebSocketWrapper {
   static instances = {};
   listeners = [];
 
-  constructor(port, comopnent_id , spinner = false) {
+  constructor(port, comopnent_id, spinner = false) {
     this.ip = window.location.hostname;
     this.port = port;
     this.comopnent_id = comopnent_id;
-    this.connect();
     this.spinner = spinner;
+    this.connect();
   }
 
-  static getInstance(port, comopnent_id , spinner = false) {
+  static getInstance(port, comopnent_id, spinner = false) {
     const key = `${port}:${comopnent_id}`;
     console.log('Getting key: ', key);
     if (!WebSocketWrapper.instances[key]) {
-      WebSocketWrapper.instances[key] = new WebSocketWrapper(port, comopnent_id , spinner);
+      WebSocketWrapper.instances[key] = new WebSocketWrapper(port, comopnent_id, spinner);
+    } else {
+      WebSocketWrapper.instances[key].cleanup();
+      WebSocketWrapper.instances[key].connect();
     }
 
     return WebSocketWrapper.instances[key];
   }
+
   hideSpinner() {
-      if(this.spinner) {
-           const spinner = document.querySelector('.spinner_custom');
-            if(spinner) {
-                document.body.removeChild(spinner);
-            }
+    if (this.spinner) {
+      const spinner = document.querySelector('.spinner_custom');
+      if (spinner) {
+        document.body.removeChild(spinner);
       }
+    }
   }
+
   showSpinner() {
-        const spinner = document.createElement('div');
-        spinner.className = 'spinner_custom';
-        document.body.appendChild(spinner);
+    const spinner = document.createElement('div');
+    spinner.className = 'spinner_custom';
+    document.body.appendChild(spinner);
   }
 
   connect() {
     let wsType = 'wss';
-    if(window.location.protocol !== "https:"){
+    if (window.location.protocol !== "https:") {
       wsType = 'ws';
     }
-  this.ws = new WebSocket(`${wsType}://${this.ip}:${this.port}/ws/${this.comopnent_id}`);
-  this.ws.binaryType = 'arraybuffer';
-  // Setting up a timeout for the initial connection
-  const connectionTimeout = setTimeout(() => {
-    if (this.ws.readyState !== WebSocket.OPEN) {
-      console.error("Connection timed out after 15 seconds.");
-      console.log("Connection timed out after 15 seconds.");
-      this.ws.close();
-    }
-  }, 15000);
+    this.ws = new WebSocket(`${wsType}://${this.ip}:${this.port}/ws/${this.comopnent_id}`);
+    this.ws.binaryType = 'arraybuffer';
 
-  this.ws.onopen = (event) => {
-    clearTimeout(connectionTimeout); // Clearing the timeout once connected
-    console.log("Connection opened:", event);
-  };
+    // Setting up a timeout for the initial connection
+    const connectionTimeout = setTimeout(() => {
+      if (this.ws.readyState !== WebSocket.OPEN) {
+        console.error("Connection timed out after 15 seconds.");
+        console.log("Connection timed out after 15 seconds.");
+        this.ws.close();
+      }
+    }, 15000);  // Reduced timeout to 15 seconds
 
-  this.ws.onerror = (error) => {
-    clearTimeout(connectionTimeout); // Clearing the timeout on error
-    console.log("WebSocket Error:", error);
-  };
+    this.ws.onopen = (event) => {
+      clearTimeout(connectionTimeout); // Clearing the timeout once connected
+      console.log("Connection opened:", event);
+    };
 
-  this.ws.onclose = (event) => {
-    console.log("Connection closed:", event);
-    setTimeout(()=>{
-      this.connect();
-    },1000)
-  };
+    this.ws.onerror = (error) => {
+      clearTimeout(connectionTimeout); // Clearing the timeout on error
+      console.log("WebSocket Error:", error);
+      this.cleanup();  // Custom method to clean up state
+      setTimeout(() => {
+        this.connect();
+      }, 1000);  // Attempt to reconnect after a short delay
+    };
 
-  this.ws.onmessage = (event) => {
-    console.log('Got message');
-    this.hideSpinner();
-    console.log(event);
-    // let jsonString = event.data;
-    // jsonString = jsonString.replace(/NaN/g, 'null');
+    this.ws.onclose = (event) => {
+      console.log("Connection closed:", event);
+      this.cleanup();  // Custom method to clean up state
+      setTimeout(() => {
+        this.connect();
+      }, 1000);  // Attempt to reconnect after a short delay
+    };
 
-    if (typeof event.data === 'string') {
-      const message = JSON.parse(event.data);
-      // console.log(`time: ${(Date.now() -start)/1000} s`)
+    this.ws.onmessage = (event) => {
+      console.log('Got message');
+      this.hideSpinner();
+      console.log(event);
+
+      if (typeof event.data === 'string') {
+        const message = JSON.parse(event.data);
         this.listeners.forEach(listener => listener(message));
-    } else {
+      } else {
         const data = msgpack.decode(new Uint8Array(event.data));
-        // console.log(`time: ${(Date.now() -start)/1000} s`)
         this.listeners.forEach(listener => listener(data));
+      }
+    };
+  }
+
+  cleanup() {
+    if (this.ws) {
+      this.ws.onopen = this.ws.onclose = this.ws.onerror = this.ws.onmessage = null;
+      this.ws = null;
     }
-
-  };
-}
-
+  }
 
   sendData(data) {
-    if(this.spinner){
-       this.showSpinner()
+    if (this.spinner) {
+      this.showSpinner();
     }
 
     const sendWhenReady = () => {
       if (this.ws.readyState === WebSocket.OPEN) {
         const jsonString = JSON.stringify(data);
         this.ws.send(jsonString);
-        // start = Date.now()
         console.log("Data sent to server:", jsonString);
       } else if (this.ws.readyState === WebSocket.CONNECTING) {
         console.warn("WebSocket connection is opening", this.comopnent_id);
-
-        // If WebSocket is still connecting, wait and then try again
-        setTimeout(sendWhenReady, 5000);
+        setTimeout(sendWhenReady, 1000);  // Reduced timeout to 1 second
       } else {
         console.error("WebSocket connection is closed, reconnecting");
         this.connect();
+        setTimeout(sendWhenReady, 1000);  // Wait a bit before retrying to ensure connection
       }
     };
 
     sendWhenReady();
-
   }
 
   addListener(listener) {
-    this.sendData({'id' : this.comopnent_id});
+    this.sendData({'id': this.comopnent_id});
     this.listeners.push(listener);
   }
 
@@ -127,4 +136,5 @@ class WebSocketWrapper {
     }
   }
 }
+
 export default WebSocketWrapper;
