751e39cb20b77adb2f84013f0563be8660d73907
lanalex <lanalex@gmail.com>
2024-11-27 04:54:05 +0200

    add tuple and bool suport (#24)
    Co-authored-by: AlexanderSeeDoo <alex@seedoo.co>

--- seedoo/streamlit/module/default_module.py
+++ seedoo/streamlit/module/default_module.py
@@ -12,18 +12,29 @@ def process_dict(value: dict, row: Any = None, disabled: bool = False) -> dict:
 def process_list(value: list, row: Any = None, disabled: bool = False) -> list:
     value.append('processed')
     return value
+
+@type_matcher(tuple, st.write)
+def process_list(value: tuple, row: Any = None, disabled: bool = False) -> list:
+    value = list(value)
+    value.append('processed')
+    return value
+
+
 @type_matcher(str, st.write)
 def process_str(value: list, row: Any = None, disabled: bool = False) -> list:
-
     return value
+
 @type_matcher(int, st.write)
 def process_int(value: list, row: Any = None, disabled: bool = False) -> list:
-
     return value
 
 @type_matcher(float, st.write)
 def process_float(value: float, row: Any = None, disabled: bool = False) -> list:
     return f"{value:.2f}"
+
+@type_matcher(bool, st.write)
+def process_float(value: bool, row: Any = None, disabled: bool = False) -> list:
+    return f"{value}"
 
 @type_matcher(type(None), st.write)
 def process_none(value: float, row: Any = None) -> list:
@@ -31,6 +42,5 @@ def process_none(value: float, row: Any = None) -> list:
 
 @column_name_matcher('list_column', st.write)
 def create_image(value: Any, row: Any = None, disabled: bool = False) -> Image.Image:
-
     return st.button(value)
 
